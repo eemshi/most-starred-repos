b2b767c248ae791862465bf663e873bfa7062218
eemshi <oreneemshi@gmail.com>
2019-12-29 00:13:50 -0700

    add /commits endpoint

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 const server = express();
 
 server.get('/most-stars', async (req, res) => {
-    const limit = req.query.limit ? req.query.limit : 100
+    const limit = req.query.limit ? req.query.limit : 100;
     try {
         const body = await axios.get(
             `https://api.github.com/search/repositories?q=stars:>=100&sort=stars&per_page=${limit}`
@@ -14,6 +14,22 @@ server.get('/most-stars', async (req, res) => {
             return _.pick(repo, ['id', 'full_name', 'url', 'stargazers_count']);
         });
         res.json(repositories);
+    } catch (err) {
+        res.status(err.response.status).end(err.response.statusText);
+    }
+});
+
+server.get('/commits', async (req, res) => {
+    const { owner, repo } = req.query;
+    const thisTimeYesterday = new Date(Date.now() - 24 * 3600 * 1000).toISOString();
+    try {
+        const body = await axios.get(
+            `https://api.github.com/repos/${owner}/${repo}/commits?since=${thisTimeYesterday}`
+        );
+        const commits = body.data.map(commit => {
+            return _.pick(commit, ['commit', 'author']);
+        });
+        res.json(commits);
     } catch (err) {
         res.status(err.response.status).end(err.response.statusText);
     }
